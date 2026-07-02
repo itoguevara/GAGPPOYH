d3e3232cebdc605589e2f1c859f9d69d4ab47ecd
itoguevara <itoguevara@gmail.com>
2026-07-02 07:50:18 -0400

    Menu

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,27 +1,31 @@
 <x-layouts::auth>
-    <div class="flex flex-col gap-6">
-        <x-auth-header :title="__('')" :description="__('Ingrese el Email y Password para iniciar sesión')" />
+  <div class="absolute left-0 top-2 w-full" >
+    <img src="../../../imgweb/logoGPP.png" alt="Guayana Productiva en Positivo" width="500" height="300">
+  </div>            
 
-        <!-- Session Status -->
-        <x-auth-session-status class="text-center" :status="session('status')" />
-
-        <form method="POST" action="{{ route('login.store') }}" class="flex flex-col gap-6">
+<div class="">
+        <form method="POST" action="{{ route('login.store') }}" >
             @csrf
-
-            <!-- Email Address -->
-            <flux:input
-                name="email"
-                :label="__('Email address')"
-                :value="old('email')"
-                type="email"
-                required
-                autofocus
-                autocomplete="email"
-                placeholder="email@example.com"
-            />
-
+            <div class="absolute left-5 top-60 w-full md:w-3/4">
+                <flux:heading size="xl">Iniciar Sesión</flux:heading>
+                <flux:text class="mt-2">Ingrese sus credenciales para acceder a su cuenta</flux:text>
+            </div>
             <!-- Password -->
-            <div class="relative">
+            <div class="absolute left-5 top-80 w-full md:w-3/4">
+                <!-- Email Address -->
+                <flux:input
+                    name="email"
+                    :label="__('Email address')"
+                    :value="old('email')"
+                    type="email"
+                    required
+                    autofocus
+                    autocomplete="email"
+                    placeholder="email@example.com"
+                />
+            </div>
+            <!-- Password -->
+            <div class="absolute left-5 top-100 w-full md:w-3/4 size-min">
                 <flux:input
                     name="password"
                     :label="__('Password')"
@@ -31,20 +35,23 @@
                     :placeholder="__('Password')"
                     viewable
                 />
-
+            </div>
+            <div class="absolute left-25 top-100 w-full">
+             <!-- Remember Me -->
+                <flux:checkbox name="remember" :label="__('Recordarme')" :checked="old('remember')" />
+            </div>
+            
                 @if (Route::has('password.request'))
-                    <flux:link class="absolute top-0 text-sm end-0" :href="route('password.request')" wire:navigate>
+                <div class="absolute left-5 top-117 w-full">
+                    <flux:link  :href="route('password.request')" wire:navigate>
                         {{ __('Recuperar contraseña') }}
                     </flux:link>
+                </div>   
                 @endif
-            </div>
 
-            <!-- Remember Me -->
-            <flux:checkbox name="remember" :label="__('Recordarme')" :checked="old('remember')" />
-
-            <div class="flex items-center justify-end">
-                <flux:button variant="primary" type="submit" class="w-full" data-test="login-button">
-                    {{ __('Iniciar sesión') }}
+            <div class="absolute left-22 top-130 w-full">
+                <flux:button  type="submit" class="boton-session" data-test="login-button">
+                    {{ __('') }}
                 </flux:button>
             </div>
         </form>
